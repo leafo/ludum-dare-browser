0dd6bc0402ed6bbd3914bf5d6608f63d69a964ec
leaf corcoran <leafot@gmail.com>
2022-04-06 14:05:48 -0700

    update dependencies, remove npx

--- static/js/require.js
+++ static/js/require.js
@@ -1,4 +1,9 @@
 
+// this module is use to provide a very basic interface for inserterting static data that can be imported
+
+// esbuild compiles with --external:ld/events, which will leave any import
+// statements alone for that module
+
 let modules = {}
 
 window.define = function(module, obj) {
